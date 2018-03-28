24dae6dacac759806c702e8f3b19aba001e48f96
rachaelhogan <31493477+rachaelhogan@users.noreply.github.com>
2018-03-27 20:48:01 -0600

    Revision 3 in branch

--- Main.java
+++ Main.java
@@ -1,10 +1,15 @@
 public class Main
 {
-    //this is the chronological revision 2 in master
+    //this is the chronological revision 3 in branch
     
     public void methodInMaster()
     {
         System.out.println("Revision 1 in master");
         System.out.println("Revision 2 in master");
+    }
+
+    public void methodInBranch()
+    {
+        System.out.println("Revision 3 in branch");
     }
 }
